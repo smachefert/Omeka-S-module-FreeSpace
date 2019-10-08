9a8ab947adcc3a3c6f7c288966af466590c1bb83
symac <web@geobib.fr>
2019-10-08 15:36:45 +0200

    First commit

---
+++ Module.php
@@ -0,0 +1,46 @@
+<?php
+namespace FreeSpace;
+
+use Omeka\Module\AbstractModule;
+use Zend\EventManager\Event;
+use Zend\EventManager\SharedEventManagerInterface;
+use Zend\View\View;
+
+class Module extends AbstractModule
+{
+    private $cssClass = "";
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    private function formattedDiskUsage() {
+	    $bytes = disk_free_space(".");
+	    $si_prefix = array( 'B', 'KB', 'MB', 'GB', 'TB', 'EB', 'ZB', 'YB' );
+	    $base = 1024;
+	    $class = min((int)log($bytes , $base) , count($si_prefix) - 1);
+
+	    if ($bytes < 5369517824) {
+	        $this->cssClass = "freeSpaceWarning";
+	    }
+
+        return sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '';
+    }
+
+    public function attachListeners(SharedEventManagerInterface $sharedEventManager)
+    {
+        $sharedEventManager->attach(
+            'Omeka\Controller\Admin\Index',
+            'view.browse.after',
+            function(Event $event) {
+                print $event->getTarget()->partial("free-space-block",
+                    [
+                        "freeSpaceFormatted" => $this->formattedDiskUsage(),
+                        "freeSpaceClass" => $this->cssClass
+                    ]
+                );
+            }
+        );
+    }
+}
---
+++ config/module.config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'view_manager' => [
+        'template_path_stack' => [
+            dirname(__DIR__) . '/view',
+        ],
+    ]
+];
